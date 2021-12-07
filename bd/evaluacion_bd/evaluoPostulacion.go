3a2faf56df126e028c27ad1ec286b48a4ce06215
jonarosero <jars9605@hotmail.com>
2021-12-06 21:19:27 -0500

    Fix: delete a class

--- bd/evaluacion_bd/evaluoPostulacion.go
+++
@@ -1,5 +0,0 @@
-package evaluacionbd
-
-import evaluacionmodels "github.com/ascendere/micro-postulaciones/models/evaluacion_models"
-
-func EvaluoPostulacion(r evaluacionmodels.EvaluacionPostulacion)
